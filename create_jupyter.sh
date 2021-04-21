10966d2545818d033900f3f2bc8cebc5a9849648
Michael J Gangler <mjgangler@gmail.com>
2021-04-21 19:35:13 -0400

    Add files via upload

---
+++ create_jupyter.sh
@@ -0,0 +1,5 @@
+conda deactivate 
+conda update conda
+conda create --name anom-detect-1 python=3.8
+conda activate anom-detect-1
+conda install ConfigParser scikit-learn matplotlib cx_oracle numpy pandas jupyter
