09236fea0d39d5a182a32a8b73084522044f885a
Michael J Gangler <mjgangler@gmail.com>
2021-04-21 19:28:19 -0400

    Add files via upload

---
+++ AnomDetect_DB.py
@@ -0,0 +1,11 @@
+#user = "ml0001"
+#pw = "7902$*OPidjid"
+#dsn = "db20210112_low"
+
+user='ml0002'
+pw ='5232)pi9OPqE'
+dsn ='db20210112_low'
+
+#user = "op1"
+#pw = "op1"
+#dsn = "10.0.1.251/prod50"
